cbd6d88097b698af408662f935f64e09a00bd611
Samantha <samanthavillafana8@gmail.com>
2024-09-16 19:34:10 -0400

    kid property and setters

--- lib/models/kid.py
+++ lib/models/kid.py
@@ -13,6 +13,31 @@ class Kid:
 
     def __repr__(self):
         return f"<Kid {self.id}: {self.name}, {self.age}>"
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and len(name):
+            self._name = name
+        else:
+            raise ValueError(
+                "Name must be a non-empty string"
+            )
+        
+    @property
+    def age(self):
+        return self._age
+    
+    @age.setter
+    def age(self, age):
+        if isinstance(age, int) and age > 0:
+            self._age = age
+        else:
+            raise ValueError("Age must be a positive integer")
+
 
     @classmethod
     def create_table(cls):
