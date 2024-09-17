85d7b23fccac063e5fe78ef92f1f057393c91cbd
Samantha <samanthavillafana8@gmail.com>
2024-09-16 20:15:10 -0400

    list all kids menu choice

--- lib/cli.py
+++ lib/cli.py
@@ -2,7 +2,7 @@
 
 from helpers import (
     exit_program,
-    helper_1
+    list_kids
 )
 
 
@@ -13,7 +13,7 @@ def main():
         if choice == "0":
             exit_program()
         elif choice == "1":
-            helper_1()
+            list_kids()
         else:
             print("Invalid choice")
 
@@ -21,7 +21,7 @@ def main():
 def menu():
     print("Please select an option:")
     print("0. Exit the program")
-    print("1. Some useful function")
+    print("1. List all kids")
 
 
 if __name__ == "__main__":
--- lib/helpers.py
+++ lib/helpers.py
@@ -2,10 +2,13 @@
 from models.kid import Kid
 from models.toy import Toy
 
-def helper_1():
-    print("Performing useful function#1.")
-
 
 def exit_program():
     print("Goodbye!")
     exit()
+
+def list_kids():
+    kids = Kid.get_all()
+    for kid in kids:
+        print(kid)
+
